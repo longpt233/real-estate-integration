a8431847b756abc2f5bbcb37b45794a55a13fff9
thanglong2000pro <laphungtien2000@gmail.com>
2022-06-23 17:11:02 +0700

    add execute_remove_duplicate

---
+++ prj-exam/data-matching/lib-match/execute_remove_duplicate.py
@@ -0,0 +1,20 @@
+from remove_duplicate import remove_duplicate
+from joblib import load
+import pandas as pd
+
+remover: remove_duplicate = load("RemoveDuplicate.lib")
+
+
+path_data = '/home/darkknight/real-estate-integration/prj-exam/schema-matching/data.csv'
+df = pd.read_csv(path_data, encoding='utf-8')
+df = df.drop(["Unnamed: 0"], axis=1)
+
+if len(df) > 0:
+    print("Original size", len(df))
+    # drop_ids = df["_id"].to_list()
+    df = remover.remove_duplicate(df)
+    print("After remove duplicates size", len(df))
+    # df.drop("_id", inplace=True, axis=1)
+    print("Save final data")
+    df.to_csv(
+        '/home/darkknight/real-estate-integration/prj-exam/data-matching/final_data.csv')
--- prj-exam/data-matching/lib-match/remove_duplicate.py
+++ prj-exam/data-matching/lib-match/remove_duplicate.py
@@ -1,6 +1,5 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
 import re
-import pymongo
 import pandas as pd
 from sklearn.metrics.pairwise import cosine_similarity
 import numpy as np
